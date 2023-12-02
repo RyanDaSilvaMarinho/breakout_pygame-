57a9c92680f1acf8c85bd8e6fcce9edd71ad3a7a
RyanDaSilvaMarinho <rdsm.snf23@uea.edu.br>
2023-12-02 13:22:06 -0400

    Criando um novo branch com meu nome e adcionando um novo código base

--- main.py
+++ main.py
@@ -1,49 +1,80 @@
 import pygame
+import sys
 
+# Inicialização do Pygame
 pygame.init()
-WIN_WIDTH, WIN_HEIGHT = 800, 600
-WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-rect = pygame.Rect(WIN_WIDTH / 2, WIN_HEIGHT / 1.1, 100, 20)
-VELOCITY = 1
-direction = {'UP': False, 'DOWN': False, 'LEFT': False, 'RIGHT': False}
 
-while True:  # main game loop
+# Configurações do jogo
+WIDTH, HEIGHT = 800, 600
+BALL_RADIUS = 10
+PADDLE_WIDTH, PADDLE_HEIGHT = 100, 10
+BRICK_WIDTH, BRICK_HEIGHT = 80, 20
+BlACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+# Inicialização da janela
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Breakout Game")
+
+# Inicialização da bola
+ball = pygame.Rect(WIDTH // 2 - BALL_RADIUS, HEIGHT // 2 - BALL_RADIUS, BALL_RADIUS * 2, BALL_RADIUS * 2)
+ball_speed = [5, 5]
+
+# Inicialização da raquete
+paddle = pygame.Rect(WIDTH // 2 - PADDLE_WIDTH // 2, HEIGHT - PADDLE_HEIGHT - 10, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+# Inicialização dos tijolos
+bricks = []
+for row in range(5):
+    for col in range(WIDTH // BRICK_WIDTH):
+        brick = pygame.Rect(col * BRICK_WIDTH, row * BRICK_HEIGHT, BRICK_WIDTH, BRICK_HEIGHT)
+        bricks.append(brick)
+
+clock = pygame.time.Clock()
+
+# Loop principal do jogo
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                direction['UP'] = True
-            elif event.key == pygame.K_DOWN:
-                direction['DOWN'] = True
-            elif event.key == pygame.K_RIGHT:
-                direction['RIGHT'] = True
-            elif event.key == pygame.K_LEFT:
-                direction['LEFT'] = True
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_UP:
-                direction['UP'] = False
-            elif event.key == pygame.K_DOWN:
-                direction['DOWN'] = False
-            elif event.key == pygame.K_RIGHT:
-                direction['RIGHT'] = False
-            elif event.key == pygame.K_LEFT:
-                direction['LEFT'] = False
+            sys.exit()
 
-    if direction['UP']:
-        rect.y -= VELOCITY
-    if direction['DOWN']:
-        rect.y += VELOCITY
-    if direction['LEFT']:
-        rect.x -= VELOCITY
-    if direction['RIGHT']:
-        rect.x += VELOCITY
+    keys = pygame.key.get_pressed()
+    paddle.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 7
 
-    if rect.left > WIN_WIDTH:
-        rect.right = WIN_WIDTH
-    if rect.top > WIN_HEIGHT:
-        rect.bottom = WIN_HEIGHT
+    # Movimentação da bola
+    ball.x += ball_speed[0]
+    ball.y += ball_speed[1]
 
-    WIN.fill((0, 0, 0))  # fill the screen with black
-    pygame.draw.rect(WIN, (255, 255, 255), rect)  # draw our rectangle
-    pygame.display.update()  # update the display
+    # Colisão da bola com as paredes
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball_speed[0] = -ball_speed[0]
+    if ball.top <= 0:
+        ball_speed[1] = -ball_speed[1]
+
+    # Colisão da bola com a raquete
+    if ball.colliderect(paddle) and ball_speed[1] > 0:
+        ball_speed[1] = -ball_speed[1]
+
+    # Colisão da bola com os tijolos
+    brick_hit = ball.collidelist(bricks)
+    if brick_hit != -1:
+        del bricks[brick_hit]
+        ball_speed[1] = -ball_speed[1]
+
+    # Verificar se o jogador perdeu
+    if ball.bottom >= HEIGHT:
+        pygame.quit()
+        sys.exit()
+
+    # Desenhar na tela
+    screen.fill(BlACK)
+    pygame.draw.ellipse(screen, BLUE, ball)
+    pygame.draw.rect(screen, WHITE, paddle)
+    for brick in bricks:
+        pygame.draw.rect(screen, RED, brick)
+
+    pygame.display.flip()
+    clock.tick(60)
